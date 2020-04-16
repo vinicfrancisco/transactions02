0adb77cb46ea28c7a0062ab37834064e44393cb8
vinicfrancisco <vini.ibi@gmail.com>
2020-04-16 11:41:33 -0300

    fixed getBalance method

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,25 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+
+    const transactions = await this.find();
+
+    const total = transactions.reduce((acumulator, currentValue) => {
+      if (currentValue.type === 'income') {
+        income += currentValue.value;
+        return acumulator + currentValue.value;
+      }
+      outcome += currentValue.value;
+      return acumulator - currentValue.value;
+    }, 0);
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 }
 
